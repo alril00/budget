167468091dce1f3f6c939db3f4f83da8edb6d453
Ria <galkina20007@gmail.com>
2025-06-15 16:06:14 +0300

    completed the functionality of the account

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-
-document.querySelectorAll('.category').forEach(category => {
-    const minus = category.querySelector('.minus')
-    const plus = category.querySelector('.plus')
-    let moneyNumber = category.querySelector('.money-input')
-
-
-    function getCurrValue() {
-        let val1 = Number(moneyNumber.value);
-        if (isNaN(val1)) return 0;
-        return val1;
-    }
-
-    function updateValue(newVal) {
-        moneyNumber.value = newVal;
-    }
-
-    plus.addEventListener('click', () => {
-        let currentValue = getCurrValue();
-        currentValue += 1000;
-        updateValue(currentValue);
-    })
-
-    minus.addEventListener('click', () => {
-        let currentValue = getCurrValue();
-        currentValue -= 1000;
-        updateValue(currentValue);
-    })
-
-})
-
 let budgetInput = document.querySelector('.input-budget')
 const budgetPlus = document.querySelector('.my-budget .plus')
 const budgetMinus = document.querySelector('.my-budget .minus')
@@ -41,18 +10,97 @@ function getBudgetValue() {
 
 function updateBudget(newVal) {
     budgetInput.value = newVal;
+    // budgetInput.value = newVal.toString().replace(/^0+/, '');
 }
 
-budgetPlus.addEventListener( 'click', () => {
+budgetPlus.addEventListener('click', () => {
     let currValue = getBudgetValue();
     currValue += 1000;
     updateBudget(currValue);
 })
 
-budgetMinus.addEventListener( 'click', () => {
+budgetMinus.addEventListener('click', () => {
     let currValue = getBudgetValue();
     currValue -= 1000;
     updateBudget(currValue);
 })
 
+
+document.querySelectorAll('.category').forEach(category => {
+    const minus = category.querySelector('.minus')
+    const plus = category.querySelector('.plus')
+    let moneyInput = category.querySelector('.money-input')
+
+
+    function getCurrNumberMoney() {
+        let val1 = Number(moneyInput.value);
+        if (isNaN(val1)) return 0;
+        return val1;
+    }
+
+    function updateValueMoney(newVal) {
+        moneyInput.value = newVal;
+    }
+
+    plus.addEventListener('click', () => {
+        let currentValue = getCurrNumberMoney();
+        currentValue += 1000;
+        updateValueMoney(currentValue);
+
+        let budgetValue = getBudgetValue();
+        budgetValue -= 1000;
+        budgetInput.value = budgetValue;
+    })
+
+    minus.addEventListener('click', () => {
+        let currentValue = getCurrNumberMoney();
+        currentValue -= 1000;
+        updateValueMoney(currentValue);
+
+        let budgetValue = getBudgetValue();
+        budgetValue += 1000;
+        budgetInput.value = budgetValue;
+
+    })
+
+
+    let isChanged = false; //flag
+
+    moneyInput.addEventListener('input', () => {
+        isChanged = true;
+    })
+
+    budgetInput.addEventListener('focus', () => {
+        let value = getBudgetValue();
+        if (value === 0) {
+            budgetInput.value = '';
+        }
+    })
+
+    moneyInput.addEventListener('focus', () => {
+        let value = getCurrNumberMoney();
+        if (value === 0) {
+            moneyInput.value = '';
+        }
+    })
+
+    document.addEventListener('click', (e) => {
+        if (isChanged && !moneyInput.contains(e.target)) {
+            let numMoney = getCurrNumberMoney();
+            updateValueMoney(numMoney);
+
+            let numBudget = getBudgetValue();
+
+            let count = numBudget - numMoney;
+            updateBudget(count);
+
+            isChanged = false;
+
+        }
+
+    })
+
+
+
+})
 
